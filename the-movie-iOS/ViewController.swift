be0a74777c390ed1ec373f729ea739303772e448
Jathinesh Kottem <Jathinesh.Kottem@tatadigital.com>
2020-11-23 20:52:15 +0530

    Initial Commit

---
+++ the-movie-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  the-movie-iOS
+//
+//  Created by Jathinesh Kottem on 23/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
