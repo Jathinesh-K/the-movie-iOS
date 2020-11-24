760e268e9faa24bee432016907bfd03ee6e93315
Jathinesh Kottem <Jathinesh.Kottem@tatadigital.com>
2020-11-24 13:12:31 +0530

    Added CollectionView grid populated with API data

---
+++ the-movie-iOS/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  the-movie-iOS
+//
+//  Created by Jathinesh Kottem on 24/11/20.
+//
+
+import Foundation
---
+++ the-movie-iOS/View/MovieCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCell.swift
+//  the-movie-iOS
+//
+//  Created by Jathinesh Kottem on 24/11/20.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
