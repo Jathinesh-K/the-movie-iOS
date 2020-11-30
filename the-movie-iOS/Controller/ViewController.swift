5b4434ade798fb9161f2219d8e355b3ab4c854d6
Jathinesh Kottem <Jathinesh.Kottem@tatadigital.com>
2020-11-30 11:44:11 +0530

    UI constraints upadted across devices and image cache implemented

--- the-movie-iOS/Controller/ViewController.swift
+++ the-movie-iOS/Controller/ViewController.swift
@@ -98,9 +98,8 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.cellIdentifier, for: indexPath) as! MovieCell
     cell.movieTitle.text = data[indexPath.row].title
     guard let posterPath = data[indexPath.row].posterPath else{return cell}
-//    cell.moviePoster.tag = indexPath.row
+    cell.moviePoster.tag = indexPath.row
     cell.moviePoster.load(url: URL(string: Constants.baseImageURL + posterPath)!)
-//    cell.moviePoster.image = UIImage(systemName: "sun.max")
     return cell
   }
   
@@ -136,29 +135,26 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
 
 extension ViewController: UICollectionViewDelegateFlowLayout{
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-    let width = (view.frame.width - 20)/2
-    let height = (view.frame.height-20)/3
-    let size = CGSize(width: width, height: height)
-    return size
+    return CGSize(width: (collectionView.frame.width - 20)/2, height: collectionView.frame.height/2.5)
   }
 }
 //MARK: - ImageView from API
 
-//let imageCache = NSCache<NSString, UIImage>()
+let imageCache = NSCache<NSString, UIImage>()
 extension UIImageView {
   func load(url: URL) {
     DispatchQueue.global().async { [weak self] in
-//      ImageCache Implementation
-//      self!.image = nil
-//      if let imageFromCache = imageCache.object(forKey: url.absoluteString as NSString) {
-//        self?.image = imageFromCache
-//        return
-//      }
+      //ImageCache Implementation
+      self!.image = nil
+      if let imageFromCache = imageCache.object(forKey: url.absoluteString as NSString) {
+        self?.image = imageFromCache
+        return
+      }
       if let data = try? Data(contentsOf: url) {
         if let image = UIImage(data: data) {
           DispatchQueue.main.async {
-//            let imageToCache = image
-//            imageCache.setObject(imageToCache, forKey: url.absoluteString as NSString)
+            let imageToCache = image
+            imageCache.setObject(imageToCache, forKey: url.absoluteString as NSString)
             self?.image = image
           }
         }
